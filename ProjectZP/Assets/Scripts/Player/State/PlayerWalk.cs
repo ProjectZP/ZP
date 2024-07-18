e0c4f3599b8cbecfbb0d81a40a2d3dfa22216261
JaeHyeok Song <jaehyeok.song0@gmail.com>
2024-07-18 17:19:52 +0900

    Initialize Player

---
+++ ProjectZP/Assets/Scripts/Player/State/PlayerWalk.cs
@@ -0,0 +1,18 @@
+using PlayerStateType = ZP.SJH.Player.PlayerStateManager.PlayerStateType;
+
+namespace ZP.SJH.Player
+{ 
+    public class PlayerWalk : PlayerState
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            _stateType = PlayerStateType.Walk;
+        }
+
+        private void FixedUpdate()
+        {
+            RegenStamina();
+        }
+    }
+}
