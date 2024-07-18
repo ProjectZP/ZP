075b04e68a536f9a2c97c895896cb86da07c16bc
lend-a-villin <lend_a_villin@naver.com>
2024-07-18 17:08:16 +0900

    더미 맵 제작 및 텔레포트 기본 기능 구현
    SampleScene - 더미 맵 및 더미 플레이어, 텔레포트 기능 확인용 트랜스폼 배치, 텔레포트 매니저 배치.
    TeleportManager - 텔레포트 기본 기능(계단 레이어 진입 시, toTransform 활성화 및 정반대 위치로 동기화) 작성 완료. 추가 기능 작성 예정(계단 레이어 벗어날 시 등).
    PlayerManager(ZP.Villin.Player) - 텔레포트 매니저 기능 확인용 더미 코드.
    StairDoorAnimationController - 계단 문 애니메이션 컨트롤러(작성 예정).

---
+++ ProjectZP/Assets/Dummy/Villin/PlayerManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace ZP.Villin.Player
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        public Action<Transform> OnEnterStair;
+        private int currentLayer;
+
+        private void Update()
+        {
+            // 임시로 레이캐스트 테스트.
+            if (Physics.Raycast(transform.position + Vector3.up, Vector3.down, out RaycastHit newHit, 2f))
+            {
+                int newLayer = newHit.collider.gameObject.layer;
+                if (currentLayer == newLayer)
+                {
+                    return;
+                }
+                currentLayer = newLayer;
+                if (currentLayer == 7)
+                {
+#if UNITY_EDITOR
+                    Debug.Log($"Stair Layer Entered!");
+#endif
+                    OnEnterStair?.Invoke(transform);
+#if UNITY_EDITOR
+                    Debug.Log(transform);
+#endif
+                }
+            }
+        }
+
+    }
+}
---
+++ ProjectZP/Assets/Scripts/Teleport/StairDoorAnimationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace ZP.Villin.Teleport
+{
+    public class StairDoorAnimationController : MonoBehaviour
+    {
+        public Action OnStairDoorClosed;
+        // 오류 안 보이도록 하는 임시 필드.
+        private bool _stairDoorClosed = false;
+        private void Update()
+        {
+        // 오류 안 보이도록 하는 임시 코드.
+            if (_stairDoorClosed)
+            {
+                OnStairDoorClosed?.Invoke();
+            }    
+            
+        }
+
+        // 오류 안 보이도록 하는 임시 메서드.
+        public void SetStairDoorClosed()
+        {
+            _stairDoorClosed = true;
+        }
+    }
+}
