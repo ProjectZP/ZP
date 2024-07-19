b736da005ed9b8ec453b4304bf15ed58a85cac30
lend-a-villin <lend_a_villin@naver.com>
2024-07-19 19:15:18 +0900

    Repair Dynamic World Sample prefab xyz problem
    Dynamic World Sample - Coordinate issue resolved.
    DynamicWorldConstructor - Added coordinate initialization feature when building worlds.
    StairDoorAnimationController - on working.

--- ProjectZP/Assets/Scripts/Teleport/StairDoorAnimationController.cs
+++ ProjectZP/Assets/Scripts/Teleport/StairDoorAnimationController.cs
@@ -5,9 +5,12 @@ namespace ZP.Villin.Teleport
 {
     public class StairDoorAnimationController : MonoBehaviour
     {
+        private ZP.Villin.Player.PlayerManager _playerManager;
         public Action OnStairDoorClosed;
         private TeleportManager _teleportManager;
+        private GameObject _collision;
         private bool _isInteractable = false;
+        private bool _isPlayerOnStair = false;
 
 
         private void Awake()
@@ -17,15 +20,40 @@ namespace ZP.Villin.Teleport
                 _teleportManager = FindFirstObjectByType<TeleportManager>();
             }
             _isInteractable = false;
-            _teleportManager.OnRemainTeleportCountZero += MakeIntaractable;
+            _playerManager.OnEnterStair += SubscribeOnEnterStair;
+            _playerManager.OnExitStair += SubscribeOnExitStair;
+            _teleportManager.OnRemainTeleportCountZero += MakeIntaractableUpstairDoor;
         }
 
-        private void MakeIntaractable()
+        private void SubscribeOnExitStair()
+        {
+            _isPlayerOnStair = false;
+        }
+
+        private void SubscribeOnEnterStair(Transform transform)
+        {
+
+            _isPlayerOnStair = true;
+        }
+
+        private void MakeIntaractableUpstairDoor()
         {
             _isInteractable = true;
 #if UNITY_EDITOR
             Debug.Log("Todo.Villin -> aMake Door Interactable!");
 #endif
+        }
+
+        private void CloseDownstairDoor()
+        {
+            if (_isPlayerOnStair == false)
+            {
+                return;
+            }
+
+            _collision.SetActive(true);
+
+            OnStairDoorClosed?.Invoke();
         }
 
         // 오류 안 보이도록 하는 임시 메서드.
